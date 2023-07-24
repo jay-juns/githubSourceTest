6d3a098eb9ab15baf6f906ebf74662251d35ded6
jay-juns <necrozerg2@naver.com>
2023-07-24 19:31:42 +0900

    Initial Commit

---
+++ githubSourceTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  githubSourceTest
+//
+//  Created by junseok Kim on 2023/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ githubSourceTest/githubSourceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubSourceTestApp.swift
+//  githubSourceTest
+//
+//  Created by junseok Kim on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct githubSourceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
